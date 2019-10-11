83b03f01134899836dd6e200c84d48eead271ee6
SirArchi <spam@pssc.pl>
2019-10-11 14:28:29 +0200

    Added filtering items. Repaired problem with default fields action.

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const addForm = document.querySelector('.add');
 const list = document.querySelector('.todos');
+const search = document.querySelector('.search input');
 
 //Func generate HTML template with new item.
 const todoTemplate = todo => {
@@ -27,4 +28,26 @@ list.addEventListener('click', ev => {
   if (ev.target.classList.contains('delete')) {
     ev.target.parentElement.remove();
   }
+});
+
+//Filter Todos  //It returns only objects with not matching to function variable.
+const filterItems = term => {
+  Array.from(list.children) //Convert from HTML to Array.
+    .filter(todo => !todo.textContent.toLowerCase().includes(term)) //Negation "includes".
+    .forEach(todo => todo.classList.add('filtered'));
+  //Back to normal class when user delete letter.
+  Array.from(list.children)
+    .filter(todo => todo.textContent.toLowerCase().includes(term))
+    .forEach(todo => todo.classList.remove('filtered'));
+};
+
+//Event Listener on keyup event.
+search.addEventListener('keyup', ev => {
+  const term = search.value.trim().toLowerCase();
+  filterItems(term);
+});
+
+//Stop default submit action on search field.
+search.parentElement.addEventListener('submit', ev => {
+  ev.preventDefault();
 });
